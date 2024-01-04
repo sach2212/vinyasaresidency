d2c50ed5731012c2f34d472cec14f15ec460d9df
Sachin <121556074+sach2212@users.noreply.github.com>
2024-01-05 02:27:01 +0530

    first commit

--- script.js
+++ script.js
@@ -10,63 +10,42 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
+ 
 
-
-var swiper = new Swiper(".home-slider", {
-    spaceBetween: 20,
-    effect: "fade",
-    grabCursor: true,
-    loop:true,
-    // autoplay: {
-    //     delay: 4500,
-    //     disableOnInteraction: false,
-    // },
+  var homeswiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
     centeredSlides: true,
+    effect: "fade",
+    rewind: true, 
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-});
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-var swiper = new Swiper(".review-slider", {
+
+var revswiper = new Swiper(".review-slider", {
     spaceBetween: 10,
     grabCursor: true,
     loop:true,
-    // autoplay: {
-    //     delay: 4500,
-    //     disableOnInteraction: false,
-    // },
+    rewind: true,
+    freeMode: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     centeredSlides: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-    },
-    // breakpoints:{
-    //     0:{
-    //         slidesPerView:1,
-    //     },
-    //     768:{
-    //         slidesPerView:2,
-    //     },
-    //     991:{
-    //         slidesPerView:3,
-    //     },
-        
-         
-    // },
-});
-
-var swiper = new Swiper(".blogs-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 4500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
     },
     breakpoints:{
         0:{
@@ -76,7 +55,11 @@ var swiper = new Swiper(".blogs-slider", {
             slidesPerView:2,
         },
         991:{
-            slidesPerView:3,
+            slidesPerView:2,
         },
+        
+         
     },
 });
+
+ 
